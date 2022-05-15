46a5494f061ee76fcd97715c63d3c7bd3b2d5f05
D0878494 <d0878494@o365.fcu.edu.tw>
2022-05-15 22:33:58 +0800

    project1

---
+++ group52/app/src/main/java/com/example/group5/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.group5;
+
+import static android.net.wifi.WifiConfiguration.Status.strings;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+//    RecyclerViewAdapter mAdapter;
+    ImageButton ib1,ib2;
+    String[] fromColumns = {"飲料專區","炸物專區","日式料理","台灣小吃","韓式料理","冰品專區","義式料理","泰式料理"};
+    int[] toViews = {R.drawable.food_drinks, R.drawable.food_fries,
+            R.drawable.food_japanese, R.drawable.food_taiwanese_snake,
+            R.drawable.food_korean,R.drawable.food_ice_cream,
+            R.drawable.food_italy, R.drawable.food_thai};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        ArrayList arrayList = new ArrayList();
+//        for (int i = 0; i < strings.length; i++) {
+//            arrayList.add(strings[i]);
+//        }
+//        mAdapter = new RecyclerViewAdapter(arrayList);
+//        recyclerView.setAdapter(mAdapter);
+
+        GridView gv = (GridView)findViewById(R.id.gv_intrduce);
+
+//        ArrayList<String> storeList = new ArrayList<String>();
+//        for (int i = 0; i < fromColumns.length; i++) {
+//            storeList.add(fromColumns[i]);
+//        }
+//        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, storeList);
+        gv.setAdapter(new ImageAdapter(this, fromColumns));
+        gv.setNumColumns(2);
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        ((TextView) v.findViewById(R.id.grid_item_label))
+                                .getText(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+//        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+////                Intent intent = new Intent();
+////                intent.setClass(MainActivity.this, MealActivity.class);
+////                intent.putExtra(STORE_NO, position);
+////                startActivity(intent);
+//
+//            }
+//        });
+    }
+
+
+}
