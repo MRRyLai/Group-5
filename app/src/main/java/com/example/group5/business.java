618dae5230d9510a1dba1a7748480e295d1ded68
D0878494 <d0878494@o365.fcu.edu.tw>
2022-06-06 16:18:38 +0800

    新增 商家接單狀況、營業狀態調整

--- app/src/main/java/com/example/group5/business.java
+++ app/src/main/java/com/example/group5/business.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.group5.shop_host;
 
@@ -16,9 +17,30 @@ public class business extends AppCompatActivity {
         setContentView(R.layout.activity_business);
     }
 
+    int a;
+
+    public void set_business(View view){
+        a=4;
+        Intent it = new Intent(this,shop_host.class );
+
+        it.putExtra("business",a);
+        startActivity(it);
+
+        Toast.makeText(this, "已將目前營業狀態設為:營業中", Toast.LENGTH_LONG).show();
+    }
+    public void set_close(View view){
+        a=5;
+        Intent it = new Intent(this,shop_host.class );
+
+        it.putExtra("business",a);
+        startActivity(it);
+
+        Toast.makeText(this, "已將目前營業狀態設為:非營業時間", Toast.LENGTH_LONG).show();
+
+    }
+
     public void go_back(View view){
-//        Intent it = new Intent(this, shop_host.class );
-//        startActivity(it);
-        finish();
+        Intent it = new Intent(this, shop_host.class );
+        startActivity(it);
     }
 }
--- app/src/main/java/com/example/group5/catch_order.java
+++ app/src/main/java/com/example/group5/catch_order.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class catch_order extends AppCompatActivity {
 
@@ -12,12 +13,42 @@ public class catch_order extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catch_order);
+
     }
 
+    int a;
+
+    public void set_busy(View view){
+        a=1;
+        Intent it = new Intent(this,shop_host.class );
+
+        it.putExtra("catch",a);
+        startActivity(it);
+
+        Toast.makeText(this, "已將目前接單狀況設為:忙碌中", Toast.LENGTH_LONG).show();
+    }
+    public void set_normal(View view){
+        a=2;
+        Intent it = new Intent(this,shop_host.class );
+
+        it.putExtra("catch",a);
+        startActivity(it);
+
+        Toast.makeText(this, "已將目前接單狀況設為:正常時段", Toast.LENGTH_LONG).show();
+    }
+    public void set_free(View view){
+        a=3;
+        Intent it = new Intent(this,shop_host.class );
+
+        it.putExtra("catch",a);
+        startActivity(it);
+
+        Toast.makeText(this, "已將目前接單狀況設為:非巔峰時段", Toast.LENGTH_LONG).show();
+
+    }
 
     public void go_back(View view){
-//        Intent it = new Intent(this,shop_host.class );
-//        startActivity(it);
-        finish();
+        Intent it = new Intent(this,shop_host.class );
+        startActivity(it);
     }
 }
