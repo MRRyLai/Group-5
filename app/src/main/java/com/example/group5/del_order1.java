044687ddf8b9a8d8a27fb42ffefa6cbb63759b3b
bubblec at <york22731858@gmail.com>
2022-06-09 23:40:42 +0800

    add full lv about the order list

--- app/src/main/java/com/example/group5/del_order1.java
+++ app/src/main/java/com/example/group5/del_order1.java
@@ -5,14 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class del_order1 extends AppCompatActivity {
+
+    String foodOrdered [] = {"辣炒年糕", "炸醬麵", "起司炸雞"};
+    String orderedAmo [] = {"*2", "*1", "*1"};
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_del_order1);
+
+        listView = (ListView) findViewById(R.id.lv_order1_list);
+        delOrderLVAdapter delOrderLVAdapter = new delOrderLVAdapter(getApplicationContext(),foodOrdered, orderedAmo);
+        listView.setAdapter(delOrderLVAdapter);
     }
 
     public void onclick(View v){
